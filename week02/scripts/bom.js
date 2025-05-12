e41bdbc78a03e4fb1398a79d7714a52c91b41b4d
sharonramirez21 <sharonramirez421@gmail.com>
2025-05-12 14:21:35 -0300

    week02 - bom example

---
+++ week02/scripts/bom.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+
+deleteButton.textContent = 'X';
+li.append(deleteButton);
+
+list.append(li);
