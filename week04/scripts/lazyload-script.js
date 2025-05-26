6bbd488ef9e7c25b4085751a27ed993142c5b001
sharonramirez21 <sharonramirez421@gmail.com>
2025-05-26 19:14:43 -0300

    week04

---
+++ week04/scripts/lazyload-script.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+const currentYear = today.getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const LastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modification: ${LastModifiedDate}`;
